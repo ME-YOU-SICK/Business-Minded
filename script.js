0235b9ed5138febb15264a83ea12af9423861b32
ME-YOU-SICK <136545118+ME-YOU-SICK@users.noreply.github.com>
2023-06-17 14:11:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,76 @@
-// script.js
+// Define an array of businesses with their properties
+const businesses = [
+  {
+    name: "Coffee Shop",
+    cost: 100,
+    incomeRate: 1,
+    level: 1
+  },
+  {
+    name: "Tech Startup",
+    cost: 500,
+    incomeRate: 5,
+    level: 1
+  },
+  {
+    name: "Fashion Boutique",
+    cost: 1000,
+    incomeRate: 10,
+    level: 1
+  },
+  // Add more businesses...
+];
 
-let money = 0;
+// Display the list of businesses in the game interface
+function displayBusinesses() {
+  const businessList = document.getElementById("business-list");
+  businessList.innerHTML = ""; // Clear previous list
+  
+  businesses.forEach((business, index) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${business.name} (Level ${business.level}) - Income: $${business.level * business.incomeRate}/s`;
+    businessList.appendChild(listItem);
 
-// Get the money display element
-const moneyDisplay = document.getElementById("money-display");
+    const buyButton = document.createElement("button");
+    buyButton.textContent = `Buy (${business.cost})`;
+    buyButton.addEventListener("click", () => buyBusiness(index));
+    listItem.appendChild(buyButton);
+  });
+}
 
-// Get the click button element
-const clickButton = document.getElementById("click-button");
+// Buy a business
+function buyBusiness(index) {
+  const business = businesses[index];
+  
+  // Check if the player has enough money to buy the business
+  if (money >= business.cost) {
+    money -= business.cost; // Deduct the cost from the player's money
+    
+    // Increase the income rate and level of the business
+    business.incomeRate++;
+    business.level++;
+    
+    displayBusinesses(); // Update the displayed businesses
+    
+    // Start generating income from the purchased business
+    setInterval(() => {
+      money += business.level * business.incomeRate;
+      updateMoneyDisplay();
+    }, 1000);
+  }
+}
 
-// Add a click event listener to the button
-clickButton.addEventListener("click", () => {
-  // Increment the money value and update the display
-  money++;
+// Update the money display in the game interface
+function updateMoneyDisplay() {
+  const moneyDisplay = document.getElementById("money-display");
   moneyDisplay.textContent = "Money: $" + money;
+}
+
+// Add event listener to the click button
+clickButton.addEventListener("click", () => {
+  money++;
+  updateMoneyDisplay();
 });
+
+// Call the displayBusinesses function to initially display the businesses
+displayBusinesses();
